a54b2d38aae4d954ba268a4261730ff78544c301
Max Holliday <maholli@stanford.edu>
2024-03-06 18:54:00 -0800

    now reprinting prior packets can take an argument file

--- rx_only/parse_prior_pckts.py
+++ rx_only/parse_prior_pckts.py
@@ -3,33 +3,51 @@
 """
 Usage:
 cd ~/PY4_gs/rx_only
+# no argument causes the script to read from default pckts
 python3 parse_prior_pckts.py
+
+# with argument reads only the argument file
+python3 parse_prior_pckts.py packet.bin
 """
 
-import time, os, binascii
+import time, os, binascii, sys
 from beacon_parse_json import parse_beacon # PY4 beacon data unpacking
 
-# packet log location
-PCKT_LOG = os.path.expanduser('~')+'/gs_pckts.txt'
-
-# read out gs_pckts file and
 prior_pckts = []
-with open(PCKT_LOG,'r') as f:
-    l=f.readline().strip()
-    while l:
-        try:
-            prior_pckts.append(eval(l))
-        except Exception as e:
-            print(f'reading error: {e}')
+
+def get_default_packets():
+    # packet log location
+    PCKT_LOG = os.path.expanduser('~')+'/gs_pckts.txt'
+
+    # read out gs_pckts file
+    with open(PCKT_LOG,'r') as f:
         l=f.readline().strip()
+        while l:
+            try:
+                p = eval(l)
+                p = binascii.unhexlify(p[3])
+                prior_pckts.append(p)
+            except Exception as e:
+                print(f'reading error: {e}')
+            l=f.readline().strip()
+
+# check if the script was started with an argument
+if len(sys.argv) > 1:
+    # read out argument file. assume tinygs format
+    with open(sys.argv[1],'rb') as f:
+        l=f.read()
+    prior_pckts.append(l)
+else:
+    # read default pckt file
+    get_default_packets()
+
 
 # for each packet, try and parse it as a PY4 beacon and print the result
 for pckt in prior_pckts:
     try:
-        beacon = pckt[3]
-        parsed_pckt = parse_beacon(binascii.unhexlify(beacon),debug=True)
+        parsed_pckt = parse_beacon(pckt,debug=True)
         # delay a small moment for dramatic effect (not needed)
         # time.sleep(0.1)
     except Exception as e:
-        print(f'parsing error: {e}')
+        print(f'parsing error: {e} {pckt}')
 
